153d938a8eba7b981492bb7fd7e6c3ef2d12094e
shah12121 <apurva1@umbc.edu>
2016-02-04 21:52:43 -0500

    insta

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+platform :ios, '8.0'
+use_frameworks!
+target 'insta' do
+
+pod 'AFNetworking', '~> 2.6'
+end
+
---
+++ insta/PhotosViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  insta
+//
+//  Created by Apurva Shah on 2/4/16.
+//  Copyright © 2016 Apurva Shah. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    let media:dictionary!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let media = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                    }
+                }
+        });
+        task.resume()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
